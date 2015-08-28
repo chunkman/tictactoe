2bfa7e188993876dee759093eced3bce2598101d
Ken <chunkman@gmail.com>
2015-08-28 05:43:35 +0000

    Added board print function

--- ttt.py
+++ ttt.py
@@ -1,16 +1,17 @@
-game_board_top = [' ','|',' ','|',' ']
-game_board_mid = [' ','|',' ','|',' ']
-game_board_bot = [' ','|',' ','|',' ']
-game_board_div = "---------"
+#game_board_top = [' ','|',' ','|',' ']
+#game_board_mid = [' ','|',' ','|',' ']
+#game_board_bot = [' ','|',' ','|',' ']
+#game_board_div = "---------"
 
-for x in game_board_top:
-    print x,
-print '\n' + game_board_div
+game_board = [[' ','|',' ','|',' '],[' ','|',' ','|',' '],[' ','|',' ','|',' ']]
+game_board_div = "--------"
 
-for x in game_board_mid:
-    print x,
+def printgameboard(board):
 
-print '\n' + game_board_div
+    for row in board:
+        for each in row:
+            print each,
+        print '\n' + game_board_div
 
-for x in game_board_bot:
-    print x,
+
+printgameboard(game_board)
