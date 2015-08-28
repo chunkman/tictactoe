396bb6906603628cf34c401dec5943014d68240d
Ken <chunkman@gmail.com>
2015-08-28 04:43:05 +0000

    Split up game board

--- ttt.py
+++ ttt.py
@@ -1,3 +1,9 @@
-print "_|_|_"
-print "_|_|_"
-print " | | "
+game_board_top = " | | "
+game_board_mid = " | | "
+game_board_bot = " | | "
+game_board_div = "-|-|-"
+print game_board_top
+print game_board_div
+print game_board_mid
+print game_board_div
+print game_board_bot
