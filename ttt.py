56de3a0793c26fd464402d39a146c19f4b3fbd7f
Ken <chunkman@gmail.com>
2015-08-28 08:50:13 +0000

    More work on input validation

--- ttt.py
+++ ttt.py
@@ -1,5 +1,5 @@
 
-game_board = [[' ','|',' ','|',' '],[' ','|',' ','|',' '],[' ','|',' ','|',' ']]
+game_board = [['7','|','8','|','9'],['4','|','5','|','6'],['1','|','2','|','3']]
 game_board_div = "----------"
 
 def printgameboard(board):
@@ -13,24 +13,28 @@ def printgameboard(board):
 
 def win_cond(board):
 
-    if board[0][0] == board[0][2] == board[0][4] != ' ':
+    if board[0][0] == board[0][2] == board[0][4]:
         return board[0][0]
-    elif board[1][0] == board[1][2] == board[1][4] != ' ':
+    elif board[1][0] == board[1][2] == board[1][4]: 
         return board[1][0]
-    elif board[2][0] == board[2][2] == board[2][4] != ' ':
+    elif board[2][0] == board[2][2] == board[2][4]:
         return board[2][0]
-    elif board[0][0] == board[1][0] == board[2][0] != ' ':
+    elif board[0][0] == board[1][0] == board[2][0]:
         return board[0][0]
-    elif board[0][2] == board[1][2] == board[2][2] != ' ':
+    elif board[0][2] == board[1][2] == board[2][2]:
         return board[0][2]
-    elif board[0][4] == board[1][4] == board[2][4] != ' ':
+    elif board[0][4] == board[1][4] == board[2][4]:
         return board[0][4]
-    elif board[0][0] == board[1][2] == board[2][4] != ' ':
+    elif board[0][0] == board[1][2] == board[2][4]:
         return board[0][0]
-    elif board[0][4] == board[1][2] == board[2][0] != ' ':
+    elif board[0][4] == board[1][2] == board[2][0]:
         return board[0][4]
     else:
         return False
+
+def check_square(square):
+    if square != 'X' and square != 'O':
+        return True
 
 def play_square(board, number, letter):
 
@@ -63,7 +67,7 @@ def play_square(board, number, letter):
         return board
 
 printgameboard(game_board)
-letter = raw_input("Do you want to be X or O?")
+letter = raw_input("Do you want to be X or O? ")
 letter = letter.upper()
 
 while (win_cond(game_board) == False):
@@ -74,8 +78,10 @@ while (win_cond(game_board) == False):
     printgameboard(game_board)
     if winning_letter:
         print "The letter %s wins!" % winning_letter
+        break
     elif letter == 'X':
         letter = 'O'
     else:
         letter = 'X'
+    print "It's %s's turn. " % letter
     
