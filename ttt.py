0788780d0e5977cb5ffa0b964137846f1c5b0d23
Ken <chunkman@gmail.com>
2015-08-28 06:50:30 +0000

    Fixed win cond function added play square function

--- ttt.py
+++ ttt.py
@@ -8,6 +8,7 @@ game_board_div = "----------"
 
 def printgameboard(board):
 
+    print '\n'
     for row in board:
         for each in row:
             print each,
@@ -16,20 +17,60 @@ def printgameboard(board):
 
 def win_cond(board):
 
-    if board[0][0] == board[0][2] == board[0][4]:
+    if board[0][0] == board[0][2] == board[0][4] != ' ':
         print "Row 1 win"
-    elif board[1][0] == board[1][2] == board[1][4]:
+    elif board[1][0] == board[1][2] == board[1][4] != ' ':
         print "Row 2 win"
-    elif board[2][0] == board[2][2] == board[2][4]:
+    elif board[2][0] == board[2][2] == board[2][4] != ' ':
         print "Row 3 win"
-    elif board[0][0] == board[1][0] == board[2][0]:
+    elif board[0][0] == board[1][0] == board[2][0] != ' ':
         print "Column 1 win"
-    elif board[0][2] == board[1][2] == board[2][2]:
+    elif board[0][2] == board[1][2] == board[2][2] != ' ':
         print "Column 2 win"
-    elif board[0][4] == board[1][4] == board[2][4]:
+    elif board[0][4] == board[1][4] == board[2][4] != ' ':
         print "Column 3 win"
-    elif board[0][0] == board[1][2] == board[2][4]:
+    elif board[0][0] == board[1][2] == board[2][4] != ' ':
         print "Diagonal 1 win"
-    elif board[0][4] == board[1][2] == board[2][0]:
+    elif board[0][4] == board[1][2] == board[2][0] != ' ':
         print "Diagonal 2 win"
+    else:
+        return False
 
+def play_square(board, number, letter):
+
+    if number == 1:
+        board[2][0] = letter
+        return board
+    elif number == 2:
+        board[2][2] = letter
+        return board
+    elif number == 3:
+        board[2][4] = letter
+        return board
+    elif number == 4:
+        board[1][0] = letter
+        return board
+    elif number == 5:
+        board[1][2] = letter
+        return board
+    elif number == 6:
+        board[1][4] = letter
+        return board
+    elif number == 7:
+        board[0][0] = letter
+        return board
+    elif number == 8:
+        board[0][2] = letter
+        return board
+    elif number == 9:
+        board[0][4] = letter
+        return board
+
+#printgameboard(game_board)
+#play_square(game_board, 1, 'X')
+#printgameboard(game_board)
+#play_square(game_board, 5, 'X')
+#printgameboard(game_board)
+#play_square(game_board, 9, 'X')
+#printgameboard(game_board)
+#win_cond(game_board)
