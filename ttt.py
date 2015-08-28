4dfcaca8e15b03bf7226e518bbea6358609cea22
Ken <chunkman@gmail.com>
2015-08-28 05:14:01 +0000

    Fixed game board

--- ttt.py
+++ ttt.py
@@ -1,9 +1,16 @@
-game_board_top = " | | "
-game_board_mid = " | | "
-game_board_bot = " | | "
-game_board_div = "-|-|-"
-print game_board_top
-print game_board_div
-print game_board_mid
-print game_board_div
-print game_board_bot
+game_board_top = [' ','|',' ','|',' ']
+game_board_mid = [' ','|',' ','|',' ']
+game_board_bot = [' ','|',' ','|',' ']
+game_board_div = "---------"
+
+for x in game_board_top:
+    print x,
+print '\n' + game_board_div
+
+for x in game_board_mid:
+    print x,
+
+print '\n' + game_board_div
+
+for x in game_board_bot:
+    print x,
