8bb3afaf632d799cd866d4a9adf043df5cc53ded
Ken <chunkman@gmail.com>
2015-08-28 08:02:13 +0000

    Added basic keyboard input

--- ttt.py
+++ ttt.py
@@ -15,20 +15,28 @@ def win_cond(board):
 
     if board[0][0] == board[0][2] == board[0][4] != ' ':
         print "Row 1 win"
+        return board[0][0]
     elif board[1][0] == board[1][2] == board[1][4] != ' ':
         print "Row 2 win"
+        return board[1][0]
     elif board[2][0] == board[2][2] == board[2][4] != ' ':
         print "Row 3 win"
+        return board[2][0]
     elif board[0][0] == board[1][0] == board[2][0] != ' ':
         print "Column 1 win"
+        return board[0][0]
     elif board[0][2] == board[1][2] == board[2][2] != ' ':
         print "Column 2 win"
+        return board[0][2]
     elif board[0][4] == board[1][4] == board[2][4] != ' ':
         print "Column 3 win"
+        return board[0][4]
     elif board[0][0] == board[1][2] == board[2][4] != ' ':
         print "Diagonal 1 win"
+        return board[0][0]
     elif board[0][4] == board[1][2] == board[2][0] != ' ':
         print "Diagonal 2 win"
+        return board[0][4]
     else:
         return False
 
@@ -63,3 +71,10 @@ def play_square(board, number, letter):
         return board
 
 
+while (win_cond(game_board) == False):
+
+    x = int(raw_input("> "))
+    letter = 'X'
+    play_square(game_board,x,letter)
+    win_cond(game_board)
+    printgameboard(game_board)
