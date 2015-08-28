1ab7e2987c076fb9257acaa2bf6fb7bbb01fc55c
Ken <chunkman@gmail.com>
2015-08-28 06:07:32 +0000

    Added win condition function

--- ttt.py
+++ ttt.py
@@ -4,7 +4,7 @@
 #game_board_div = "---------"
 
 game_board = [[' ','|',' ','|',' '],[' ','|',' ','|',' '],[' ','|',' ','|',' ']]
-game_board_div = "--------"
+game_board_div = "----------"
 
 def printgameboard(board):
 
@@ -12,6 +12,21 @@ def printgameboard(board):
         for each in row:
             print each,
         print '\n' + game_board_div
+    print '\n'
 
+def win_cond(board):
+
+    if board[0][0] == board[0][2] == board[0][4]:
+        print "Row 1 win"
+    elif board[1][0] == board[1][2] == board[1][4]:
+        print "Row 2 win"
+    elif board[2][0] == board[2][2] == board[2][4]:
+        print "Row 3 win"
+
+game_board[0][0] = 'X'
+game_board[0][2] = 'X'
+game_board[0][4] = 'X'
 
 printgameboard(game_board)
+
+win_cond(game_board)
