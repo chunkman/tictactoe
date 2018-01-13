36893bfb571c25e0b711aa19f6000d3433a10dd8
Ken Allen <chunkman@gmail.com>
2018-01-13 04:19:18 -0500

    Removed un-needed comments

--- newttt.py
+++ newttt.py
@@ -19,11 +19,6 @@ def player1_play():
 
     player1_space = int(player1_space)
 
-    #while(player1_space < 1 or player1_space > 9):
-    #    
-    #    print "Please enter a number between 1 and 9."
-    #    player1_space = int(raw_input("X: Enter space number: "))
-    
     game_board[player1_space-1] = player1_letter
     value_board[player1_space-1] = player1_value
     
@@ -38,10 +33,6 @@ def player2_play():
         player2_space = raw_input("O: Enter space number: ")
 
     player2_space = int(player2_space)
-
-    #while(player2_space < 1 or player2_space > 9):
-    #    print "Please enter a number between 1 and 9."
-    #    player2_space = int(raw_input("O: Enter space number: "))
 
     game_board[player2_space-1] = player2_letter
     value_board[player2_space-1] = player2_value
@@ -95,6 +86,3 @@ while(True):
         break
     
 print_game_board()
-
-   # print "score_board = ", score_board
-   # print "value_board = ", value_board
