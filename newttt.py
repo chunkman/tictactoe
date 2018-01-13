44afbee40aca966f62637d9cc9df84ecb44627a5
Ken Allen <chunkman@gmail.com>
2018-01-13 02:43:35 -0500

    Added test game loop

--- newttt.py
+++ newttt.py
@@ -9,32 +9,42 @@ class Piece(object):
         else:
             self.value = 4
         
-player1_piece = Piece('X',5)
-player2_piece = Piece('O',3)
 game_board = ['1','2','3','4','5','6','7','8','9']
 score_board = [0,0,0,0,0,0,0,0]
 value_board = [0,0,0,0,0,0,0,0,0]
 
-game_board[player1_piece.space] = player1_piece.letter
-value_board[player1_piece.space] = player1_piece.value
-game_board[player2_piece.space] = player2_piece.letter
-value_board[player2_piece.space] = player2_piece.value
+player1_letter = 'X'
+player2_letter = 'O'
 
-score_board[0] = value_board[0] + value_board[4] + value_board[8]
-score_board[1] = value_board[0] + value_board[3] + value_board[6]
-score_board[2] = value_board[1] + value_board[4] + value_board[7]
-score_board[3] = value_board[2] + value_board[5] + value_board[8]
-score_board[4] = value_board[2] + value_board[4] + value_board[6]
-score_board[5] = value_board[0] + value_board[1] + value_board[2]
-score_board[6] = value_board[3] + value_board[4] + value_board[5]
-score_board[7] = value_board[6] + value_board[7] + value_board[8]
+while(True):
 
+    player1_space = int(raw_input("Enter space number: "))
+    player1_piece = Piece(player1_letter,player1_space)
 
-print game_board[0] + '|' + game_board[1] + '|' + game_board[2] + '\n'
-print '-----------------------' + '\n'
-print game_board[3] + '|' + game_board[4] + '|' + game_board[5] + '\n'
-print '-----------------------' + '\n'
-print game_board[6] + '|' + game_board[7] + '|' + game_board[8] + '\n'
+    player2_space = int(raw_input("Enter space number: "))
+    player2_piece = Piece(player2_letter,player2_space)
 
-print "score_board = ", score_board
-print "value_board = ", value_board
+    game_board[player1_piece.space] = player1_piece.letter
+    value_board[player1_piece.space] = player1_piece.value
+    game_board[player2_piece.space] = player2_piece.letter
+    value_board[player2_piece.space] = player2_piece.value
+
+    score_board[0] = value_board[0] + value_board[4] + value_board[8]
+    score_board[1] = value_board[0] + value_board[3] + value_board[6]
+    score_board[2] = value_board[1] + value_board[4] + value_board[7]
+    score_board[3] = value_board[2] + value_board[5] + value_board[8]
+    score_board[4] = value_board[2] + value_board[4] + value_board[6]
+    score_board[5] = value_board[0] + value_board[1] + value_board[2]
+    score_board[6] = value_board[3] + value_board[4] + value_board[5]
+    score_board[7] = value_board[6] + value_board[7] + value_board[8]
+
+    print "-----------------------"
+    print game_board[0] + '|' + game_board[1] + '|' + game_board[2]
+    print '-----------------------' 
+    print game_board[3] + '|' + game_board[4] + '|' + game_board[5] 
+    print '-----------------------' 
+    print game_board[6] + '|' + game_board[7] + '|' + game_board[8] 
+    print '-----------------------' + '\n'
+
+    print "score_board = ", score_board
+    print "value_board = ", value_board
